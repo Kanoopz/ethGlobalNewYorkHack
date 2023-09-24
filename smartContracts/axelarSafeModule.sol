9953d61b938c0ed889e8bedd664bad5aeed5b77f
Carlos Alberto Arroyo Gonzalez <43384993+Kanoopz@users.noreply.github.com>
2023-09-24 05:24:44 -0600

    Add files via upload

---
+++ smartContracts/axelarSafeModule.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0 <0.9.0;
+
+import { AxelarExecutable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol';
+import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
+import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
+import { IERC20 } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol';
+
+
+
+interface GnosisSafe
+{
+    enum Operation
+    {
+        Call,
+        DelegateCall
+    }       
+
+
+    function execTransactionFromModule(address to, uint256 value, bytes memory data, Operation operation) external returns (bool success);
+}
+
+
+contract axelarSafeModule is AxelarExecutable
+{
+    address public myAaAddress;
+    IAxelarGasService public immutable gasService;
+
+
+
+    constructor(address paramAaAddress, address gateway_, address gasReceiver_) AxelarExecutable(gateway_) 
+    {
+        myAaAddress = paramAaAddress;
+        gasService = IAxelarGasService(gasReceiver_);
+    }
+
+
+
+
+     // Call this function to update the value of this contract along with all its siblings'.
+    function makeCcAaOrder(string calldata destinationChain, string calldata destinationAddress, address to, bytes memory payload) external payable 
+    {
+        require(msg.value > 0, 'Gas payment is required');
+
+        bytes memory data = abi.encode(to, payload);
+        gasService.payNativeGasForContractCall{ value: msg.value }(address(this), destinationChain, destinationAddress, data, msg.sender);
+
+        gateway.callContract(destinationChain, destinationAddress, data);
+    }
+
+    // Handles calls created by setAndSend. Updates this contract's value
+    function _execute(string calldata sourceChain_, string calldata sourceAddress_, bytes calldata payload_) internal override 
+    {
+        address to;
+        uint256 value;
+        bytes memory data;
+
+        (to, data) = abi.decode(payload_, (address, bytes));
+
+        GnosisSafe(myAaAddress).execTransactionFromModule(to, 0, data, GnosisSafe.Operation.Call);
+    }
+}
---
+++ smartContracts/hyperSafeModule.sol
@@ -0,0 +1,358 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0 <0.9.0;
+
+// hyperlaneImports  //////////
+import {IInterchainSecurityModule, ISpecifiesInterchainSecurityModule} from "./imports/IInterchainSecurityModule.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import { ByteHasher } from './imports/ByteHasher.sol';
+
+// lenImports        //////////
+import "./imports/lens/LensDataTypes.sol";
+
+
+interface GnosisSafe
+{
+    enum Operation
+    {
+        Call,
+        DelegateCall
+    }       
+
+
+    function execTransactionFromModule(address to, uint256 value, bytes memory data, Operation operation) external returns (bool success);
+}
+
+interface profileProxy
+{
+    function proxyCreateProfile(DataTypes.CreateProfileData memory vars) external;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//      hyperlaneInterfaces                                                   //////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+// sendInterfaces      //////////
+interface IMailbox 
+{
+    function dispatch(
+        uint32 _destination,
+        bytes32 _recipient,
+        bytes calldata _body
+    ) external returns (bytes32);
+}
+
+interface IInterchainGasPaymaster 
+{
+    /**
+     * @notice Deposits msg.value as a payment for the relaying of a message
+     * to its destination chain.
+     * @dev Overpayment will result in a refund of native tokens to the _refundAddress.
+     * Callers should be aware that this may present reentrancy issues.
+     * @param _messageId The ID of the message to pay for.
+     * @param _destinationDomain The domain of the message's destination chain.
+     * @param _gasAmount The amount of destination gas to pay for.
+     * @param _refundAddress The address to refund any overpayment to.
+     */
+    function payForGas(
+        bytes32 _messageId,
+        uint32 _destinationDomain,
+        uint256 _gasAmount,
+        address _refundAddress
+    ) external payable;
+
+    /**
+     * @notice Quotes the amount of native tokens to pay for interchain gas.
+     * @param _destinationDomain The domain of the message's destination chain.
+     * @param _gasAmount The amount of destination gas to pay for.
+     * @return The amount of native tokens required to pay for interchain gas.
+     */
+    function quoteGasPayment(uint32 _destinationDomain, uint256 _gasAmount)
+        external
+        view
+        returns (uint256);
+}
+
+// receiveInterfaces   //////////
+interface IMessageRecipient 
+{
+    function handle(
+        uint32 _origin, //Domain id of the sender chain
+        bytes32 _sender,
+        bytes calldata _body
+    ) external;
+}
+
+interface Messenger 
+{
+    function sendMessage(
+        address _target,
+        bytes memory _message,
+        uint32 _gasLimit
+    ) external;
+}
+
+
+// hyperErc20          //////////
+interface hyperErc20
+{
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    function hyperTransfer
+    (
+        uint32 paramDestinationChainDomain, 
+        address paramDestinationChainAddressReceiver,
+        address paramDestinationAddressToTransfer,
+        uint256 paramQuantityToTransfer
+    ) 
+    external payable;
+}
+
+
+
+
+
+
+
+contract hyperSafeModule
+{
+    using ByteHasher for bytes;
+
+
+
+    event receivedCcMessage(address to, uint256 value, bytes data);
+    event ccAaOrderExecuted(address to, uint256 value, bytes data);
+
+
+    address public myAaAddress;
+
+    address public ccCallerAddres;
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //      hyperlane                                                             //////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    // sendVariables       //////////
+    uint256 gasAmount = 300000;
+    address MAILBOX = 0xCC737a94FecaeC165AbCf12dED095BB13F037685; //SAME ON ALL CHAINS.///
+    IInterchainGasPaymaster igp = IInterchainGasPaymaster(0x8f9C3888bFC8a5B25AED115A82eCbb788b196d2a); ////// iGasPaymaster on thisChain (default, same all chains) /////
+
+    // receiveVariables    //////////
+     IInterchainSecurityModule public interchainSecurityModule = IInterchainSecurityModule(address(0));
+
+     ////////////////////////////////////////////////////////////////////////////////////////
+    //      lens                                                                   //////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    profileProxy profileCreater = profileProxy(0x420f0257D43145bb002E69B14FF2Eb9630Fc4736);
+    address profileProxyAddress = 0x420f0257D43145bb002E69B14FF2Eb9630Fc4736;
+
+
+
+
+
+
+
+
+
+    constructor(address paramAaAddress)
+    {
+        myAaAddress = paramAaAddress;
+    }
+
+    function executeChainAbstractionOrder(address paramCrossChainTo, uint256 paramCrossChainValue, bytes memory paramCrossChainData) public
+    {
+        /*
+        contract AbstractB {
+            function getX() returns(uint);
+        }
+
+        contract A {
+            function makeCall(address addressB) {
+                AbstractB(addressB).getX.value(1 ether).gas(0);
+            }
+        }
+        */
+
+        GnosisSafe(myAaAddress).execTransactionFromModule(paramCrossChainTo, paramCrossChainValue, paramCrossChainData, GnosisSafe.Operation.Call);
+        
+        
+        emit ccAaOrderExecuted(paramCrossChainTo, paramCrossChainValue, paramCrossChainData);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //      hyperlane                                                             //////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    
+    /////////////////////////////
+    // sendFuncs       //////////
+    /////////////////////////////
+    function addressToBytes32(address _addr) internal pure returns (bytes32) 
+    {
+        return bytes32(uint256(uint160(_addr)));
+    }
+
+    function makeCcCall
+    (
+        uint32 paramDestinationChainDomain, 
+        address paramDestinationChainAddressReceiver,
+        address paramCalldataTo,
+        uint256 paramCalldataValue,
+        bytes memory paramCalldataData
+    ) 
+    public payable
+    {
+        bytes memory encodedValue = abi.encode(paramCalldataTo, paramCalldataValue, paramCalldataData);
+
+        bytes32 messageId = IMailbox(MAILBOX).dispatch(
+            paramDestinationChainDomain,
+            addressToBytes32(paramDestinationChainAddressReceiver),
+            //abi.encode(paramCalldataTo, paramCalldataValue, paramCalldataData)
+            encodedValue
+        );
+
+        igp.payForGas{ value: msg.value }(
+            messageId, // The ID of the message that was just dispatched
+            paramDestinationChainDomain, // The destination domain of the message
+            1200000,
+            address(tx.origin) // refunds are returned to transaction executer
+        );
+    }
+
+    function makeLensProfileCcCall
+    (
+        uint32 paramDestinationChainDomain, 
+        address paramDestinationChainAddressReceiver,
+        address paramOtherChainAbstractedAccount,
+        string calldata paramHandle
+    ) 
+    external payable
+    {
+        DataTypes.CreateProfileData memory structData = DataTypes.CreateProfileData(paramOtherChainAbstractedAccount, paramHandle, "nothing", address(0), bytes("nothing"), "nothing");
+
+        bytes memory callData = abi.encodeCall(profileProxy.proxyCreateProfile, (structData));
+
+        bytes memory encodedValue = abi.encode(profileProxyAddress, 0, callData);
+
+
+        bytes32 messageId = IMailbox(MAILBOX).dispatch(
+            paramDestinationChainDomain,
+            addressToBytes32(paramDestinationChainAddressReceiver),
+            //abi.encode(profileProxyAddress, 0, callData)
+            encodedValue
+        );
+
+        igp.payForGas{ value: msg.value }(
+            messageId, // The ID of the message that was just dispatched
+            paramDestinationChainDomain, // The destination domain of the message
+            12000000,
+            address(tx.origin) // refunds are returned to transaction executer
+        );
+    }
+
+    receive() external payable 
+    {}
+
+    function deposit() external payable 
+    {}
+
+    /////////////////////////////
+    // receiveFuncs    //////////
+    /////////////////////////////
+    modifier onlyMailbox() 
+    {
+        require(msg.sender == MAILBOX);
+        _;    
+    }
+
+
+
+    function bytes32ToAddress(bytes32 _buf) internal pure returns (address) 
+    {
+        return address(uint160(uint256(_buf)));
+    }
+
+    function handle(uint32 _origin, bytes32 _sender, bytes calldata _body) external onlyMailbox 
+    {
+        //require(gasleft() >= 7000000000, "not enoughGas");
+
+        ccCallerAddres = bytes32ToAddress(_sender);
+
+        address receivedCcTo;
+        uint256 receivedCcValue;
+        bytes memory receivedCcData;
+
+        (receivedCcTo, receivedCcValue, receivedCcData) = abi.decode(_body, (address, uint256, bytes));
+        
+
+        emit receivedCcMessage(receivedCcTo, receivedCcValue, receivedCcData);
+        
+        executeChainAbstractionOrder(receivedCcTo, receivedCcValue, receivedCcData);
+
+
+        /*
+            ccCallerAddres = bytes32ToAddress(_sender);
+
+            address paramNumberScAddress;
+            uint paramArbitraryNumber;
+
+            (paramNumberScAddress, paramArbitraryNumber) = abi.decode(_body, (address, uint));
+
+            (bool success, ) = paramNumberScAddress.call(abi.encodeWithSignature("setNumber(uint256)", paramArbitraryNumber));
+
+        /*
+            originChainSender = paramOriginChainSender;
+            arbitraryNumber = paramArbitraryNumber;
+
+            originChain = _origin;
+        */
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //      hyperlane                                                             //////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    function chainAbstractionErc20Transfer 
+    (
+        address paramThisChainAddressHyperErc20,
+        uint32 paramDestinationChainDomain, 
+        address paramDestinationChainAddressHyperErc,
+        address paramAddressToTransfer,
+        uint256 paramQunatityToTransferFromThisChain,
+        address otherChainAaModuleAddress,
+        uint256 paramQunatityToTransferFromOtherChain
+    )
+    public
+   {
+        bytes memory hyperTransferCalldataData = abi.encodeWithSignature("hyperTransfer(uint32,address,address,uint256)", paramDestinationChainDomain, paramDestinationChainAddressHyperErc, paramAddressToTransfer, paramQunatityToTransferFromThisChain);
+
+        executeChainAbstractionOrder(paramThisChainAddressHyperErc20, 0, hyperTransferCalldataData);
+
+        /*
+        hyperErc20(paramThisChainAddressHyperErc20).hyperTransfer
+        (
+            paramDestinationChainDomain, 
+            paramDestinationChainAddressHyperErc,
+            paramAddressToTransfer,
+            paramQunatityToTransferFromThisChain
+        );
+        */
+
+        bytes memory paramCalldataData = abi.encodeWithSignature("transfer(address,uint256)", paramAddressToTransfer, paramQunatityToTransferFromOtherChain);
+
+        //.call(abi.encodeWithSignature("setNumber(uint256)", paramNumber));
+
+        //function transfer(address to, uint256 amount) public virtual override returns (bool)
+
+        makeCcCall
+        (
+            paramDestinationChainDomain, 
+            otherChainAaModuleAddress,
+            paramDestinationChainAddressHyperErc,
+            0,
+            paramCalldataData
+        );
+   }
+}
